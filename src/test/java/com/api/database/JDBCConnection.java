8c2211f8a4c30ea0c8c19f36d4ada5d9dc60096e
ketan.sethi <ketansethi786@gmail.com>
2018-10-27 18:35:40 +0530

    database connection

---
+++ src/test/java/com/api/database/JDBCConnection.java
@@ -0,0 +1,28 @@
+package com.api.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCConnection {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+		// TODO Auto-generated method stub
+
+		String host = "localhost";
+		String port = "3306";
+		Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/db_example", "root",
+				"root");
+		Statement s = con.createStatement();
+
+		ResultSet rs = s.executeQuery("SELECT * FROM TRADE_LETTER;");
+		while (rs.next()) {
+			System.out.println(rs.getString("TRADE_LETTER_ID"));
+		}
+
+	}
+
+}
