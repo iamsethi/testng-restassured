96936d7517ad66e40b735dad018fc31492df3e2f
Ketan Sethi <ketansethi786@gmail.com>
2018-10-29 19:31:14 +0530

    test

---
+++ Readme.md
@@ -0,0 +1,18 @@
+http://jsonviewer.stack.hu/
+java -jar rest.jar --server.port=8085
+https://developer.paypal.com/developer/applications/create
+https://apps.twitter.com/
+jsoupExampleSpringSecurity.war credentials are : user and user
+302 code means successful redirection
+
+
+http://www.javadoc.io/doc/io.rest-assured/rest-assured/3.0.0
+
+###################################################################
+LAPTOP - 
+
+docker run -d -p 8888:8080 cloudesire/tomcat:7-jre8
+
+Mange->Service->Apache Tomcat->start
+
+localhost:8888 -> Go to Manage App -> 
---
+++ src/main/java/com/api/paypalexamples/base/BaseClassoauth2.java
@@ -0,0 +1,37 @@
+package com.api.paypalexamples.base;
+
+//Static Imports
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.RestAssured;
+
+public class BaseClassoauth2 {
+	
+	public	static String accessToken;
+	
+	//PUT YOUR CLIENT ID & CLIENT SECRET HERE  https://developer.paypal.com/developer/applications/create
+	public static final String  clientId="AVOTONjTN6MpB3La6q1Kp_Csuwbn5xJA3QVDSJTO0-U8mF5mOaZ2uSyY7hs5mkB-wjz-8eQ3wU9iRx_7";
+	public static  final String  clientSecret="EBDZTgp9LD7Te3wz3ysl1PD8HPBjVFkCl9XupTDpDZful06rotwc_EcqKbyDH971QoH_r__8eUMtoMHs";
+
+	@BeforeClass
+	public static void init(){
+		
+		RestAssured.baseURI="https://api.sandbox.paypal.com";
+		RestAssured.basePath="/v1";
+	
+		accessToken=	given()
+				.params("grant_type","client_credentials")
+				.auth()
+				.preemptive()
+				.basic(clientId, clientSecret)
+				.when()
+				.post("/oauth2/token")
+				.then()
+				.extract()
+				.path("access_token");
+		 System.out.println("The token is: "+accessToken);
+	}
+	
+}
