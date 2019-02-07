d0366e1d15cd84fa7cf25179f42025e324f4f754
ketan.sethi <ketansethi786@gmail.com>
2019-02-07 12:49:06 +0530

    Spotify API

--- src/test/java/com/api/assertions/AssertionsExamples.java
+++ src/test/java/com/api/assertions/AssertionsExamples.java
@@ -5,12 +5,12 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
-import static org.hamcrest.Matchers.hasEntry;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,12 +18,13 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 
 public class AssertionsExamples {
-	static final String APIKEY = "38456a8097msh8b40ea11c2cad67p1f4175jsn50cae3bbd864";
-
+	static final String OAUTH_TOKEN = "BQCnMIuTpOxydEkxChZ-rHUBT9lwoIz4K8CQ-fCoZ6BFcEf8tA_30ZU4ouBaesj52_BcF5Hm_eNLqJPmSAPotJ4yaIgLXUOedzvf8xNWbvTn0VuX5wvXMBk2_PtHjddNA3aY7DOTNSebKtmLzqm56BeNdmMv3zCr1g";
+	//generate OAUTH_TOKEN manually
+	//https://developer.spotify.com/console/get-artist-albums/?id=0TnOYISbd1XYRBk9myaseg&include_groups=single%2Cappears_on&market=ES&limit=10&offset=5
 	@BeforeClass
 	public static void init() {
-
-		RestAssured.baseURI = "https://trains.p.rapidapi.com/";
+		RestAssured.baseURI = "https://api.spotify.com";
+		RestAssured.basePath = "/v1";
 	}
 	
 
@@ -31,114 +32,149 @@ public class AssertionsExamples {
 	@Test
 	public void test001() {
 		given()
-		.header("Content-Type", "application/json")
-		.header("X-RapidAPI-Key", APIKEY)
+		.header("Accept", "application/json")
+		.header("Content-Type","application/json")
+		.auth()
+		.oauth2(OAUTH_TOKEN)
+		.queryParam("market", "ES")
+		.queryParam("limit", "10")
+		.queryParam("offset", "5")
 		.when()
-		.body("{\"search\":\"Rajdhani\"}")
-		.post()
+		.get("/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks")
 		.then()
-			.body("train_num.size()", equalTo(6));
+		.body("items.size()", equalTo(10));
 		
 	}
 
-	// 2) Verify Query
-	@Test
-	public void test002() {
-		given()
-		.header("Content-Type", "application/json")
-		.header("X-RapidAPI-Key", APIKEY)
-		.when()
-		.body("{\"search\":\"Rajdhani\"}")
-		.post()
-		.then()
-		.body("train_from[0]", equalToIgnoringCase("dbrg"));
-	}
-
-
-	// 3) Check Single Name in ArrayList
-	@Test
-	public void test003() {
-		given()
-		.header("Content-Type", "application/json")
-		.header("X-RapidAPI-Key", APIKEY)
-		.when()
-		.body("{\"search\":\"Rajdhani\"}")
-		.post()
-		.then()
-		.body("train_from",hasItem("NDLS"));}
-
-	// 4) Check Multiple Names in ArrayList
-	@Test
-	public void test004() {
-		given()
-		.header("Content-Type", "application/json")
-		.header("X-RapidAPI-Key", APIKEY)
-		.when()
-		.body("{\"search\":\"Rajdhani\"}")
-		.post()
-		.then()
-		.body("train_from",hasItems("NDLS","DBRG"));}
-
 	
-	// 5) Verify the gift options for the first product (Checking Values inside Map using hasValue())
+	// 2) Verify Query
 		@Test
-		public void test005() {
+		public void test002() {
 			given()
-			.header("Content-Type", "application/json")
-			.header("X-RapidAPI-Key", APIKEY)
+			.header("Accept", "application/json")
+			.header("Content-Type","application/json")
+			.auth()
+			.oauth2(OAUTH_TOKEN)
+			.queryParam("market", "ES")
+			.queryParam("limit", "10")
+			.queryParam("offset", "5")
 			.when()
-			.body("{\"search\":\"Rajdhani\"}")
-			.post()
+			.get("/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks")
 			.then()
-			.body("data[0]", hasKey("classes"));
-			}
+			.body("href", equalToIgnoringCase("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks?offset=5&limit=10&market=ES"));
+		}
 
 
+		// 3) Check Single Name in ArrayList
+		@Test
+		public void test003() {
+			given()
+			.header("Accept", "application/json")
+			.header("Content-Type","application/json")
+			.auth()
+			.oauth2(OAUTH_TOKEN)
+			.queryParam("market", "ES")
+			.queryParam("limit", "10")
+			.queryParam("offset", "5")
+			.when()
+			.get("/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks")
+			.then()
+			.body("items.name",hasItem("Party Ain't Over"));
+		}
 
-	// 6) Check hashmap values inside a list
+		// 4) Check Multiple Names in ArrayList
+		@Test
+		public void test004() {
+			given()
+			.header("Accept", "application/json")
+			.header("Content-Type","application/json")
+			.auth()
+			.oauth2(OAUTH_TOKEN)
+			.queryParam("market", "ES")
+			.queryParam("limit", "10")
+			.queryParam("offset", "5")
+			.when()
+			.get("/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks")
+			.then()
+			.body("items.name",hasItems("Party Ain't Over","Drinks for You (Ladies Anthem)"));
+		}
+		
+		// 5) Verify the gift options for the first product (Checking Values inside Map using hasValue())
+			@Test
+			public void test005() {
+				given()
+				.header("Accept", "application/json")
+				.header("Content-Type","application/json")
+				.auth()
+				.oauth2(OAUTH_TOKEN)
+				.queryParam("market", "ES")
+				.queryParam("limit", "10")
+				.queryParam("offset", "5")
+				.when()
+				.get("/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks")
+				.then()
+				.body("items[0].artists[0]", hasKey("uri"));
+				}
+
+		// 6) Check hashmap values inside a list
 		@Test
 		public void test006() {
 			given()
-			.header("Content-Type", "application/json")
-			.header("X-RapidAPI-Key", APIKEY)
+			.header("Accept", "application/json")
+			.header("Content-Type","application/json")
+			.auth()
+			.oauth2(OAUTH_TOKEN)
+			.queryParam("market", "ES")
+			.queryParam("limit", "10")
+			.queryParam("offset", "5")
 			.when()
-			.body("{\"search\":\"Rajdhani\"}")
-			.post()
+			.get("/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks")
 			.then()
-			.body("data.findAll{it.id=='7974'}", hasItems(hasEntry("id", "7974")));
+			.body("items.findAll{it.name=='Get It Started'}", hasItems(hasEntry("name","Get It Started")));
 					
 		}
 		
-	// 7) Checking multiple values in the same statement
-	  @Test
+
+		// 7) Checking multiple values in the same statement
+		@Test
 		public void test007() {
 			given()
-			.header("Content-Type", "application/json")
-			.header("X-RapidAPI-Key", APIKEY)
+			.header("Accept", "application/json")
+			.header("Content-Type","application/json")
+			.auth()
+			.oauth2(OAUTH_TOKEN)
+			.queryParam("market", "ES")
+			.queryParam("limit", "10")
+			.queryParam("offset", "5")
 			.when()
-			.body("{\"search\":\"Rajdhani\"}")
-			.post()
+			.get("/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks")
 			.then()
-			.body("data.findAll{it.id=='7974'}", hasItems(hasEntry("id", "7974")))
-			.body("data.id",hasItem("7974"))
+			.body("items.findAll{it.name=='Get It Started'}", hasItems(hasEntry("name", "Get It Started")))
+			.body("items.name",hasItem("Get It Started"))
 			.statusCode(200);
 		}
-	// 8) Logical Assertions
-	@Test
-	public void test008() {
-		given()
-		.header("Content-Type", "application/json")
-		.header("X-RapidAPI-Key", APIKEY)
-		.when()
-		.body("{\"search\":\"Rajdhani\"}")
-		.post()
-		.then()
-		.body("train_num.size()",equalTo(6))
-		.body("train_num.size()",greaterThan(5))
-		.body("train_num.size()",lessThan(11))
-		.body("train_num.size()",greaterThanOrEqualTo(6))
-		.body("train_num.size()",lessThanOrEqualTo(6));
-			
+
+		// 8) Logical Assertions
+		@Test
+		public void test008() {
+			given()
+			.header("Accept", "application/json")
+			.header("Content-Type","application/json")
+			.auth()
+			.oauth2(OAUTH_TOKEN)
+			.queryParam("market", "ES")
+			.queryParam("limit", "10")
+			.queryParam("offset", "5")
+			.when()
+			.get("/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks")
+			.then()
+			.body("items.size()",equalTo(10))
+			.body("items.size()",greaterThan(5))
+			.body("items.size()",lessThan(11))
+			.body("items.size()",greaterThanOrEqualTo(10))
+			.body("items.size()",lessThanOrEqualTo(10));
+				
+		}
+
 	}
 
-}
